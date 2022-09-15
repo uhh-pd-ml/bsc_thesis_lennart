1e9622712e333a39cb42aa27de56f58e6b72c055
Lennart Kämmle <18233294+lennart-k@users.noreply.github.com>
2022-09-15 20:23:56 +0200

    naive linear interpolation: Sort by pt and align eta, phi

--- amazing_ai/interpol/linear.py
+++ amazing_ai/interpol/linear.py
@@ -1,14 +1,25 @@
 from typing import Callable, Optional
 import numpy as np
 
+def align_events(a: np.ndarray, b: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    n_particles = max(len(a), len(b))
+    larger_event = a if len(a) > len(b) else b
+    a_aligned = np.concatenate((a, np.zeros((n_particles-len(a), 3))))
+    b_aligned = np.concatenate((b, np.zeros((n_particles-len(b), 3))))
+    a_aligned[len(a):, 1:3] = larger_event[len(a):, 1:3]
+    b_aligned[len(b):, 1:3] = larger_event[len(b):, 1:3]    
+    return a_aligned, b_aligned
+
 def interpol_linear(a: np.ndarray, b: np.ndarray, n_points: int, radius: Optional[float] = None) -> tuple[Callable[[int], np.array]]:
     """
     Per-particle linear interpolation
     """
+    # Sort both events by pt
+    a = a[np.argsort(a[:, 0])][::-1]
+    b = b[np.argsort(b[:, 0])][::-1]
 
-    n_particles = max(len(a), len(b))
-    a = np.concatenate([a, np.zeros((n_particles-len(a), 3))])
-    b = np.concatenate([b, np.zeros((n_particles-len(b), 3))])
+    a, b = align_events(a, b)
+
     distance = np.linalg.norm(a - b)
     
     def _interpolate(step: float):
